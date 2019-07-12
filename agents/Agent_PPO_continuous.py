497db18b9a4014399f9c547c2ca8abac0920cc24
edoardo <phate09@hotmail.it>
2019-07-12 19:35:07 +0100

    created class for continuous PPO

--- agents/Agent_PPO_continuous.py
+++ agents/Agent_PPO_continuous.py
@@ -1,13 +1,12 @@
-import random
 from collections import deque
 
 import numpy as np
-import progressbar as pb
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from scipy import stats
 
 import constants
-import pong_utils
 from agents.GenericAgent import GenericAgent
 
 
@@ -37,11 +36,7 @@ class AgentPPO(GenericAgent):
         self.model: torch.nn.Module = config[constants.model]
         self.optimiser: torch.optim.Optimizer = config[constants.optimiser]
         self.ending_condition = config[constants.ending_condition]
-
-        self.state_list = []
-        self.action_list = []
-        self.prob_list = []
-        self.reward_list = []
+        self.n_games = 1
 
     # self.t_update_target_step = 0
     def required_properties(self):
@@ -57,13 +52,6 @@ class AgentPPO(GenericAgent):
                 constants.optimiser,
                 constants.ending_condition]
 
-    def collect(self, state, action, probs, reward):
-        self.state_list.append(state)
-        self.action_list.append(action)
-        self.prob_list.append(probs)
-        self.reward_list.append(reward)
-        pass
-
     def act(self, state=None):
         """Returns actions for given state as per current policy.
 
@@ -75,7 +63,7 @@ class AgentPPO(GenericAgent):
 
         return np.random.rand(self.action_size)
 
-    def learn(self):
+    def learn(self, state_list, prob_list, reward_list, epsilon, beta):
         """Update value parameters using given batch of experience tuples.
 
         Params
@@ -83,21 +71,11 @@ class AgentPPO(GenericAgent):
             experiences (Tuple[torch.Variable]): tuple of (s, a, r, s', done) tuples
             gamma (float): discount factor
         """
-        rewards = self.reward_list.copy()
-        rewards.reverse()
-        previous_rewards = 0
-        for i in range(len(rewards)):
-            rewards[i] = rewards[i] + self.discount * previous_rewards
-            previous_rewards = rewards[i]
-        rewards.reverse()
-        rewards_standardised = stats.zscore(rewards, axis=1)
-        rewards_standardised = np.nan_to_num(rewards_standardised, False)
-        assert not np.isnan(rewards_standardised).any()
-        rewards_standardised = torch.tensor(rewards_standardised, dtype=torch.float, device=self.device)
-        states = torch.stack(self.state_list)
-        output = self.model(states)
-
-        pass
+        for s in range(self.sgd_iterations):
+            L = -self.clipped_surrogate(self.model, prob_list, state_list, reward_list, epsilon=epsilon, beta=beta)
+            self.optimiser.zero_grad()
+            L.backward()
+            self.optimiser.step()
 
     def reset(self):
         """Reset the memory of the agent"""
@@ -105,58 +83,6 @@ class AgentPPO(GenericAgent):
         self.action_list = []
         self.prob_list = []
         self.reward_list = []
-
-    def train_OpenAI(self, env, writer):
-        epsilon = self.epsilon
-        beta = self.beta
-
-        # keep track of progress
-        mean_rewards = []
-        # widget = ['training loop: ', pb.Percentage(), ' ',
-        #           pb.Bar(), ' ', pb.ETA()]
-        timer = pb.ProgressBar(maxval=self.n_episodes).start()
-        for i_episode in range(self.n_episodes):
-
-            # collect trajectories
-            old_probs, states, actions, rewards = pong_utils.collect_trajectories(env, self.model, tmax=self.max_t)
-
-            total_rewards = np.sum(rewards, axis=0)
-
-            # gradient ascent step
-            for _ in range(self.sgd_iterations):
-                # uncomment to utilize your own clipped function!
-                L = -self.clipped_surrogate(self.model, old_probs, states, actions, rewards, epsilon=epsilon, beta=beta)
-                # L = -pong_utils.clipped_surrogate(policy, old_probs, states, actions, rewards, epsilon=epsilon, beta=beta)
-                self.optimiser.zero_grad()
-                L.backward()
-                self.optimiser.step()
-                del L
-
-            # the clipping parameter reduces as time goes on
-            epsilon *= .999
-
-            # the regulation term also reduces
-            # this reduces exploration in later runs
-            beta *= .995
-
-            # get the average reward of the parallel environments
-            mean = np.mean(total_rewards)
-            writer.add_scalar('data/score', np.max(total_rewards), i_episode)
-            writer.add_scalar('data/score_average', np.mean(total_rewards), i_episode)
-            result = {"mean": mean}
-            mean_rewards.append(mean)
-            if self.ending_condition(result):
-                print("ending condition met!")
-                break
-            # display some progress every 20 iterations
-            # if (i_episode + 1) % 20 == 0:
-            #     print("Episode: {0:d}, score: {1:f}".format(i_episode + 1, np.mean(total_rewards)))
-            #     print(total_rewards)
-
-            # update progress widget bar
-            timer.update(i_episode + 1)
-
-        timer.finish()
 
     def train(self, env, brain_name, writer, ending_condition, n_episodes=2000, max_t=1000):
         """
@@ -171,24 +97,51 @@ class AgentPPO(GenericAgent):
         """
         scores = []  # list containing scores from each episode
         scores_window = deque(maxlen=100)  # last 100 scores
-
+        epsilon = self.epsilon
+        beta = self.beta
         for i_episode in range(n_episodes):
             env_info = env.reset(train_mode=True)[brain_name]  # reset the environment
-            self.reset()  # reset the agent
+            # Reset the memory of the agent
+            state_list = []
+            action_list = []
+            prob_list = []
+            reward_list = []
             state = env_info.vector_observations[0]  # get the current state
             score = 0
             for t in range(max_t):
-                action, probs = self.act(state)
+                action, log_prob, entropy = self.act(state)
                 env_info = env.step(action)[brain_name]  # send the action to the environment
                 next_state = env_info.vector_observations[0]  # get the next state
                 reward = env_info.rewards[0]  # get the reward
                 done = env_info.local_done[0]  # see if episode has finished
-                self.collect(state, action, reward, next_state, done)
+                state_list.append(state)
+                action_list.append(action)
+                prob_list.append(log_prob)
+                reward_list.append(reward)
                 state = next_state
                 score += reward
                 if done:
                     break
-            self.learn()
+            # prepares the rewards
+            rewards = self.reward_list.copy()
+            rewards.reverse()
+            previous_rewards = 0
+            for i in range(len(rewards)):
+                rewards[i] = rewards[i] + self.discount * previous_rewards
+                previous_rewards = rewards[i]
+            rewards.reverse()
+            rewards_standardised = stats.zscore(rewards, axis=1)
+            rewards_standardised = np.nan_to_num(rewards_standardised, False)
+            assert not np.isnan(rewards_standardised).any()
+            rewards_standardised = torch.tensor(rewards_standardised, dtype=torch.float, device=self.device)
+            reward_list = rewards_standardised
+            self.learn(state_list, prob_list, reward_list, epsilon=epsilon, beta=beta)
+            # the clipping parameter reduces as time goes on
+            epsilon *= .999
+
+            # the regulation term also reduces
+            # this reduces exploration in later runs
+            beta *= .995
             scores_window.append(score)  # save most recent score
             scores.append(score)  # save most recent score
             writer.add_scalar('data/score', score, i_episode)
@@ -204,10 +157,10 @@ class AgentPPO(GenericAgent):
                 break
         return scores
 
-    def clipped_surrogate(self, policy, old_probs, states, actions, rewards,
-                          discount=0.995, epsilon=0.1, beta=0.01):
-        actions = torch.tensor(actions, dtype=torch.int8, device=self.device)
-        old_probs = torch.tensor(old_probs, dtype=torch.float, device=self.device)
+
+    def clipped_surrogate(self, old_probs, states, rewards, discount=0.995, epsilon=0.1, beta=0.01):
+        # actions = torch.tensor(actions, dtype=torch.int8, device=self.device)
+        old_log_probs = torch.tensor(old_probs, dtype=torch.float, device=self.device)
         rewards.reverse()
         previous_rewards = 0
         for i in range(len(rewards)):
@@ -220,18 +173,30 @@ class AgentPPO(GenericAgent):
         rewards_standardised = torch.tensor(rewards_standardised, dtype=torch.float, device=self.device)
 
         # convert states to policy (or probability)
-        new_probs = pong_utils.states_to_prob(policy, states)
-        new_probs = torch.where(actions == pong_utils.RIGHT, new_probs, 1.0 - new_probs)
+        actions, log_prob, entropy = self.model(states)
 
         # cost = torch.log(new_probs) * rewards_standardised
-        ratio = new_probs / old_probs
+        ratio = torch.exp(log_prob - old_log_probs)
         cost = torch.min(ratio, torch.clamp(ratio, 1 - epsilon, 1 + epsilon)) * rewards_standardised
-        # include a regularization term
-        # this steers new_policy towards 0.5
-        # which prevents policy to become exactly 0 or 1
-        # this helps with exploration
-        # add in 1.e-10 to avoid log(0) which gives nan
-        entropy = -(new_probs * torch.log(old_probs + 1.e-10) + (1.0 - new_probs) * torch.log(1.0 - old_probs + 1.e-10))
-
         my_surrogate = torch.mean(cost + beta * entropy)
         return my_surrogate
+
+
+class Policy(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, output_dim)
+        self.std = nn.Parameter(torch.ones(output_dim))
+
+    def forward(self, x):
+        output = F.relu(self.fc1(x))
+        output = F.tanh(self.fc2(output))
+        output = self.fc3(output)
+        dist = torch.distributions.Normal(output, F.softplus(self.std))
+        actions = dist.sample()
+        log_prob = dist.log_prob(actions)
+        log_prob = torch.sum(log_prob, dim=-1)
+        entropy = torch.sum(dist.entropy(), dim=-1)
+        return actions, log_prob, entropy
